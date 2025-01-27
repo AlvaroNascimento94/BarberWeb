0cfbc4a1ce3840d176ff9ed6b006fd476556e1e3
AlvaroNascimento94 <alvarosum@hotmail.com>
2025-01-27 10:55:31 -0300

    Feat: Model Listing Function

--- src/pages/haircuts/HairCut.tsx
+++ src/pages/haircuts/HairCut.tsx
@@ -1,16 +1,65 @@
 import { Link } from "react-router-dom";
 import { SideBar } from "../../components/sideBar/SideBar";
-import {
-  Button,
-  Flex,
-  Heading,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
+import { Button, Flex, Heading, Stack, Text } from "@chakra-ui/react";
 import { IoPricetag } from "react-icons/io5";
 import { Switch } from "../../components/ui/switch";
+import { api } from "@/services/apiClient";
+import { useEffect, useState, ChangeEvent } from "react";
+import { parseCookies } from "nookies";
+import { setupAPIClient } from "@/services/api";
+interface HairCutItem {
+  id: string;
+  name: string;
+  price: string;
+  status: boolean;
+  user_Id: string;
+}
 
-export default function HairCuts() {
+interface HairCutProps {
+  haircuts: HairCutItem[];
+}
+
+export default function HairCuts({ haircuts }: HairCutProps) {
+  const [haircutsList, setHaircutsList] = useState<HairCutItem[]>(
+    haircuts || []
+  );
+  const [disabled, setDisabled] = useState("enabled");
+
+  async function handleDisabled(e) {
+    const apiClient = setupAPIClient();
+    if(e.target.value === "disabled"){
+      setDisabled("enabled")
+      const response = await apiClient.get("/haircuts", {
+        params: {
+          status: "true",
+        },
+      })
+      setHaircutsList(response.data)
+    }else{
+      setDisabled("disabled")
+      const response = await apiClient.get("/haircuts", {
+        params: {
+          status: "false",
+        },
+      })
+      setHaircutsList(response.data)
+    }
+  }
+
+  async function hairCutList() {
+    const { "@barber.token": token } = parseCookies();
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+    const response = await api.get("/haircuts", {
+      params: {
+        status: "true",
+      },
+    });
+    setHaircutsList(response.data);
+    console.log(response.data);
+  }
+  useEffect(() => {
+    hairCutList();
+  }, []);
 
   return (
     <>
@@ -46,30 +95,52 @@ export default function HairCuts() {
               <Text color="white" fontWeight="bold">
                 ATIVOS
               </Text>
-              <Switch colorPalette="green" size="lg" />
+              <Switch
+                colorPalette="green"
+                size="lg"
+                value={disabled}
+                onChange={(e) => {
+                  handleDisabled(e);
+                }}
+                checked={disabled === "disabled"? false : true}
+              />
             </Stack>
           </Flex>
-          <Link to="/haircuts/1" style={{ width: "100%" }}>
-            <Flex
-              cursor={"pointer"}
-              w="100%"
-              p={4}
-              bg="var(--barber-400)"
-              direction={['column', 'row']}
-              alignItems={['flex-start', 'center']}
-              rounded={4}
-              mb={2}
-              justifyContent={"space-between"}
+          {haircutsList?.map((haircut) => (
+            <Link
+              key={haircut.id}
+              to={`/haircuts/${haircut.id}`}
+              style={{ width: "100%" }}
             >
-              <Flex direction="row" alignItems="center" mb={[3 ,0]} justifyContent="center" rounded={10}>
-                <IoPricetag size={28} color=" #fba931" />
-                <Text mr={4} ml={4} color="white" fontWeight="bold">
-                  corte completo
+              <Flex
+                cursor={"pointer"}
+                w="100%"
+                p={4}
+                bg="var(--barber-400)"
+                direction={["column", "row"]}
+                alignItems={["flex-start", "center"]}
+                rounded={4}
+                mb={2}
+                justifyContent={"space-between"}
+              >
+                <Flex
+                  direction="row"
+                  alignItems="center"
+                  mb={[3, 0]}
+                  justifyContent="center"
+                  rounded={10}
+                >
+                  <IoPricetag size={28} color=" #fba931" />
+                  <Text mr={4} ml={4} color="white" fontWeight="bold">
+                    {haircut.name}
+                  </Text>
+                </Flex>
+                <Text color={"white"} fontWeight={"bold"}>
+                  Preço: R$ {haircut.price}
                 </Text>
               </Flex>
-              <Text color={'white'} fontWeight={'bold'  }>Preço: R$ 50,00</Text>
-            </Flex>
-          </Link>
+            </Link>
+          ))}
         </Flex>
       </SideBar>
     </>
